35912bdd4f6616afca0ede11056a137b4089075b
unknown <p.mobedi@gmail.com>
2024-03-08 02:49:34 -0800

    BottomTabNavigator

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+export default function HomeScreen() {
+    return(
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+}
